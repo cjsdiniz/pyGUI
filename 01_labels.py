59eb35e14743bb240152fbb3b9de6a3647d9431e
Carlos Diniz <67067863+cjsdiniz@users.noreply.github.com>
2023-05-11 14:02:06 +0100

    added CPI Awareness

--- 01_labels.py
+++ 01_labels.py
@@ -3,3 +3,16 @@ from tkinter import ttk
 from windows import set_dpi_awareness
 
 
+set_dpi_awareness()
+
+root=tk.Tk()
+root.geometry("800x600")
+root.resizable(False,False)
+root.title("Widget Examples")
+
+lbl1 = ttk.Label(root,text="Widget Example",padding=20)
+#lbl1.config(font=("Arial",20))
+lbl1.config(font=("Segoe UI",20))
+lbl1.pack()
+
+root.mainloop()
